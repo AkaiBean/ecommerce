5945ffb76d2535ed09266f90cc16b98331177748
AkaiBean <rodneyrhkk@gmail.com>
2021-11-18 13:00:56 -0800

    finish sign in handleSubmit method

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,32 @@ const firebaseConfig = {
 	measurementId: 'G-D15V4E4ES5'
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+	if (!userAuth) {
+		return;
+	}
+
+	const userRef = firestore.doc(`users/${userAuth.uid}`);
+	const snapShot = await userRef.get();
+	if (!snapShot.exists) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...additionalData
+			});
+		} catch (e) {
+			console.log('error creating user', e.message);
+		}
+	}
+
+	return userRef;
+};
+
 firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
